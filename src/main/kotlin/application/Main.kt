83969925a487d7b6b732c5ed606a75b53322817f
DORIDAV <dorian.davi@thoughtworks.com>
2023-06-12 15:11:09 +0200

    removed comment

--- src/main/kotlin/application/Main.kt
+++ src/main/kotlin/application/Main.kt
@@ -8,11 +8,13 @@ fun main() {
     val cart = Cart(mutableListOf())
     val product = Product("Apple Pencil")
     val product2 = Product("Sony Wireless headphone")
-    cart.items.addAll(arrayOf(Item(product, 1), Item(product2, 2)))
+    val product3 = Product("Sony Wireless headphone new")
+
+    cart.items.addAll(arrayOf(Item(product, 1), Item(product2, 2), Item(product3, 1)))
+
+    cart.items.remove(Item(product3, 1))
     println("Cart = $cart")
     val items: List<Item> = cart.items
-
-
 
     println("----------------------------------------")
     println("products = $items")
--- src/main/kotlin/domain/Cart.kt
+++ src/main/kotlin/domain/Cart.kt
@@ -1,7 +1,3 @@
 package domain
 
-data class Cart(val items: MutableList<Item>) {
-
-    //fun quantity(): Map<String, Int> = items.groupingBy { it.product.name }.eachCount()
-
-}
+data class Cart(val items: MutableList<Item>)
