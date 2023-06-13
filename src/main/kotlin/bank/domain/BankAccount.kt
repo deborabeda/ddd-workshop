3d24b42618b7e29a295ad24693bfdd7d30e23c44
doriancodes <dorian.alo.davi@gmail.com>
2023-06-13 16:32:27 +0200

    added some comments

--- src/main/kotlin/bank/domain/BankAccount.kt
+++ src/main/kotlin/bank/domain/BankAccount.kt
@@ -2,8 +2,10 @@ package bank.domain
 
 import java.util.*
 
+//aggregate
 data class BankAccount(val id: UUID = UUID.randomUUID(), var address: Address) {
 
+    //should only be accessible through customer
     fun updateAddress(address: Address) {
         this.address = address
     }
--- src/main/kotlin/bank/domain/Customer.kt
+++ src/main/kotlin/bank/domain/Customer.kt
@@ -2,6 +2,7 @@ package bank.domain
 
 import java.util.*
 
+//aggregate and aggregate root
 data class Customer(val id: UUID = UUID.randomUUID(), val accounts: List<BankAccount>, var address: Address) {
 
     fun updateAddress(address: Address) {
